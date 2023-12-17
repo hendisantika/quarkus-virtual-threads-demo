fd17af95dd5fcd9fc1d85822d9a573c9d1d14e0a
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 19:39:08 +0700

    Add testInitialItems in TodoResourceTest🫸🌀✏️📗🐧🐳

--- src/test/java/com/hendisantika/TodoResourceTest.java
+++ src/test/java/com/hendisantika/TodoResourceTest.java
@@ -1,10 +1,21 @@
 package com.hendisantika;
 
+import com.hendisantika.virtualthreads.entity.Todo;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit5.virtual.ShouldNotPin;
 import io.quarkus.test.junit5.virtual.VirtualThreadUnit;
+import jakarta.ws.rs.core.MediaType;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +32,19 @@ import org.junit.jupiter.api.TestMethodOrder;
 @VirtualThreadUnit
 @ShouldNotPin
 public class TodoResourceTest {
+    @Test
+    @Order(1)
+    void testInitialItems() {
+        List<Todo> todos = get("/api").then()
+                .statusCode(HttpStatus.SC_OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .extract().body().as(getTodoTypeRef());
+        assertEquals(4, todos.size());
+
+        get("/api/1").then()
+                .statusCode(HttpStatus.SC_OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body("title", is("Introduction to Quarkus"))
+                .body("completed", is(true));
+    }
 }
