8a8d5f08ce505322f7b1bda047a68fde8a0a113d
hendisantika <hendisantika@yahoo.co.id>
2023-12-17 16:36:38 +0700

    Add getAll in TodoController🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/virtualthreads/controller/TodoController.java
+++ src/main/java/com/hendisantika/virtualthreads/controller/TodoController.java
@@ -1,8 +1,13 @@
 package com.hendisantika.virtualthreads.controller;
 
+import com.hendisantika.virtualthreads.entity.Todo;
 import io.quarkus.logging.Log;
+import io.quarkus.panache.common.Sort;
 import io.smallrye.common.annotation.RunOnVirtualThread;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,12 @@ public class TodoController {
     private void log() {
         Log.infof("Called on %s", Thread.currentThread());
     }
+
+    @GET
+    public List<Todo> getAll() {
+        log();
+        // pinTheCarrierThread(); // To demonstrate pinning detection
+        return Todo.listAll(Sort.by("order"));
+    }
+
 }
